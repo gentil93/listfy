2288b466eec1b2e0be216fd37dcf56d3769274b6
Gentil <matheus.gentil@dev.getmore.com.br>
2019-01-27 00:38:55 +0000

    created the structure

---
+++ src/main/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { bindActionCreators } from 'redux'
+require('./app.css')
+
+class App extends React.Component<any, any> {
+    render() {
+	    return (
+            <h1>ReactJS with Typescript</h1>
+        )
+    }
+}
+export default App
